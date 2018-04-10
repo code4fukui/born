2909fce1ae6cb8cbe2820e4290aa8c01ecb73cb4
Taisuke Fukuno <fukuno@TPC0129.local>
2018-04-10 11:15:40 +0900

    support PWA

---
+++ serviceworker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "born_cache";
+
+var addCache = function(url) {
+	caches.open(CACHE_NAME).then(function(cache) {
+		return fetch(new Request(url)).then(function(response) {
+			if (response.ok) {
+//				console.log("ok!");
+				cache.put(response.url, response);
+				evt.respondWith(response);
+			} else {
+//				console.log("NG...");
+//				return Promise.reject('Invalid response. URL:' + response.url + ' Status: ' + response.status);
+			}
+		});
+	});
+};
+
+self.addEventListener("fetch", function(evt) {
+	var url = evt.request.url;
+//	console.log("URL: " + url);
+	var res = caches.match(evt.request, { cacheName: CACHE_NAME })
+	if (res) {
+//		console.log("cache hit!");
+		evt.respondWith(res);
+	} else {
+//		console.log("no hit!");
+		addCache(url);
+	}
+});
